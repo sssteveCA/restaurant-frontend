9a4d8384916f76bb7c7401e44d9660e8a09b5754
steven <steven@StefanoPugg>
2023-03-27 17:07:53 +0200

    Added leftMenu and rightMenu methods to Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,24 +2,24 @@
 import React, { Component, useState } from 'react'
 import './Navbar.sass'
 
-export default class Navbar extends Component{
+export default class Navbar extends React.Component<any,any>{
 
     private readonly menu_data  = [
-        {title: 'Home', link: '#'},
-        {title: 'Chi siamo', link: '#'},
-        {title: 'Menu', link: '#'},
-        {title: 'Blog', link: '#'},
-        {title: 'Contatti', link: '#'},
+        {id: 0, title: 'Home', link: '#'},
+        {id: 1, title: 'Chi siamo', link: '#'},
+        {id: 2, title: 'Menu', link: '#'},
+        {id: 3, title: 'Blog', link: '#'},
+        {id: 4, title: 'Contatti', link: '#'},
     ];
 
     private readonly menu_not_logged = [
-        {title: 'Login', link: '#'},
-        {title: 'Registrati', link: '#'},
+        {id: 5,title: 'Login', link: '#'},
+        {id: 6,title: 'Registrati', link: '#'},
     ]
 
     private readonly menu_logged = [
-        {title: 'Profilo', link: '#'},
-        {title: 'Logout', link: '#'},
+        {id: 7,title: 'Profilo', link: '#'},
+        {id: 8,title: 'Logout', link: '#'},
     ]
 
     constructor(props: any){
@@ -30,20 +30,43 @@ export default class Navbar extends Component{
     }
 
     public render(){
-
         return (
             <nav>
                 <ul className="flex bg-black">
-                {
-                    this.menu_data.map((menu_item, index) => {
-                        return <li key={index} className="mr-6 px-3 py-3">
-                            <a className="text-blue-500 hover:text-blue-800" href={menu_item.link}>{menu_item.title}</a>
-                        </li>
-                    })
-                }
+                    {this.leftMenu()}
+                    {this.rightMenu()}
                 </ul>
             </nav>     
           )
+    }
+
+    private leftMenu(): Array<JSX.Element> {
+        return this.menu_data.map((menu_item) => {
+            return (
+                    <li key={menu_item.id} className="mr-6 px-3 py-3">
+                        <a 
+                            className="text-blue-500 hover:text-blue-800" 
+                            href={menu_item.link}>{menu_item.title}</a>
+                    </li>
+            )
+        });
+    }
+
+    private rightMenu(): Array<JSX.Element>{
+        let right_menu = [];
+        if(this.state.logged === true)
+            right_menu = this.menu_logged;
+        else
+            right_menu = this.menu_not_logged;
+        return right_menu.map(menu_item => {
+            return (
+                <li key={menu_item.id} className="mr-6 px-3 py-3">
+                    <a 
+                        className="text-blue-500 hover:text-blue-800" 
+                        href={menu_item.link}>{menu_item.title}</a>
+                </li>
+            )
+        })
     }
   
 }
