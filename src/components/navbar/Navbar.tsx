7e8f88ce123db60cab15e09f21fdd9e940eb093e
steven <steven@StefanoPugg>
2023-03-27 20:24:15 +0200

    Added collapse and menu methods to Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -33,11 +33,36 @@ export default class Navbar extends React.Component<any,any>{
 
     render(): React.ReactNode {
         return (
-            <nav className='flex flex-col lg:flex-row lg:justify-between bg-black'>
-                <ul className='flex flex-col lg:flex-row'>{this.leftMenu()}</ul>
-                <ul className='flex flex-col lg:flex-row'>{this.rightMenu()}</ul>
+            <nav className='bg-black text-white'>
+                <div className="container flex flex-wrap items-center justify-between mx-auto">
+                    {this.collapse()}
+                    <div className="flex flex-col lg:flex-row lg:justify-between bg-black">
+                        {this.menu()}
+                    </div>
+                </div>
+                
             </nav>     
           )
+    }
+
+    private collapse(): JSX.Element{
+        return (
+            <>
+                <div>Online Restaurant</div>
+                <button data-collapse-toggle="navbar-online-restaurant" type="button" className="inline-flex py-5 px-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-online-restaurant" aria-expanded="false">
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                </button>
+            </>
+        );
+    }
+
+    private menu(): JSX.Element{
+        return (
+            <div className="hidden w-full" id="navbar-online-restaurant">
+                <ul className='flex flex-col lg:flex-row'>{this.leftMenu()}</ul>
+                <ul className='flex flex-col lg:flex-row'>{this.rightMenu()}</ul>
+            </div>
+        );
     }
 
     /**
