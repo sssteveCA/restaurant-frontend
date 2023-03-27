48c365265344621d12607f3e6b457f624cfeabd5
steven <steven@StefanoPugg>
2023-03-27 15:16:25 +0200

    Added src/components/navbar/Navbar.sass

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,28 @@
 
 import React from 'react'
+import './Navbar.sass'
 
 export const Navbar = () => {
+
+    const menu_data  = [
+        {title: 'Home', link: '#'},
+        {title: 'Chi siamo', link: '#'},
+        {title: 'Menu', link: '#'},
+        {title: 'Blog', link: '#'},
+        {title: 'Contatti', link: '#'},
+    ];
   return (
-    <ul className="flex">
-  <li className="mr-6">
-    <a className="text-blue-500 hover:text-blue-800" href="#">Active</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-blue-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-blue-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
-  </li>
-</ul>
+    <nav>
+        <ul className="flex bg-black">
+        {
+            menu_data.map(menu_item => {
+                return <li className="mr-6 px-3 py-3">
+                    <a className="text-blue-500 hover:text-blue-800" href={menu_item.link}>{menu_item.title}</a>
+                </li>
+            })
+        }
+        </ul>
+    </nav>
+    
   )
 }
