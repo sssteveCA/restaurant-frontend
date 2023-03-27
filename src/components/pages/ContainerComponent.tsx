9367938d130172211fea91eda9e82d167fae943a
steven <steven@StefanoPugg>
2023-03-28 00:16:55 +0200

    Added src/compoments/pages/not_found/NotFoundComponent.tsx

--- src/components/pages/ContainerComponent.tsx
+++ src/components/pages/ContainerComponent.tsx
@@ -6,6 +6,7 @@ import AboutComponent from "./about/AboutComponent";
 import RestaurantMenu from "./restaurant_menu/RestaurantMenu";
 import BlogComponent from "./blog/BlogComponent";
 import ContactsComponent from "./contacts/ContactsComponent";
+import NotFoundComponent from "./not_found/NotFoundComponent";
 
 export default class ContainerComponent extends Component<any,any>{
 
@@ -21,7 +22,8 @@ export default class ContainerComponent extends Component<any,any>{
                     <Route path="about" element={<AboutComponent />} />  
                     <Route path="menu" element={<RestaurantMenu />} />    
                     <Route path="blog" element={<BlogComponent />} />
-                    <Route path="contacts" element={<ContactsComponent />} />           
+                    <Route path="contacts" element={<ContactsComponent />} />
+                    <Route path="*" element={<NotFoundComponent />} />        
                 </Routes>    
             </div>
         )
---
+++ src/components/pages/not_found/NotFoundComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundComponent = () => {
+  return (
+    <div className='text-2xl md:text-3xl lg:text-4xl text-black font-black text-center'>Pagina non trovata</div>
+  )
+}
+
+export default NotFoundComponent
