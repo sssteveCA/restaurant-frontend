8503ade340b015328a29006d3fdebda570a71649
steven <steven@StefanoPugg>
2023-03-27 20:37:58 +0200

    Added onCollapseButtonClick method to Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -31,12 +31,19 @@ export default class Navbar extends React.Component<any,any>{
         
     }
 
+    private onCollapseButtonClick(): void{
+        let menu = document.getElementById('navbar-online-restaurant');
+        if(menu)
+            menu.classList.toggle('hidden');
+
+    }
+
     render(): React.ReactNode {
         return (
             <nav className='bg-black text-white'>
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     {this.collapse()}
-                    <div className="flex flex-col lg:flex-row lg:justify-between bg-black">
+                    <div className="flex flex-col lg:flex-row lg:justify-between bg-black w-full">
                         {this.menu()}
                     </div>
                 </div>
@@ -45,20 +52,28 @@ export default class Navbar extends React.Component<any,any>{
           )
     }
 
+    /**
+     * The portion of the menu with the button to collapse the menu in small devices
+     * @returns 
+     */
     private collapse(): JSX.Element{
         return (
             <>
-                <div>Online Restaurant</div>
-                <button data-collapse-toggle="navbar-online-restaurant" type="button" className="inline-flex py-5 px-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-online-restaurant" aria-expanded="false">
+                <div className='lg:hidden'>Online Restaurant</div>
+                <button data-collapse-toggle="navbar-online-restaurant" type="button" className="inline-flex py-5 px-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 lg:hidden" aria-controls="navbar-online-restaurant" aria-expanded="false" onClick={()=>{ this.onCollapseButtonClick()}}>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
             </>
         );
     }
 
+    /**
+     * The container for all menu items
+     * @returns 
+     */
     private menu(): JSX.Element{
         return (
-            <div className="hidden w-full" id="navbar-online-restaurant">
+            <div id="navbar-online-restaurant" className="hidden lg:flex lg:flex-row lg:justify-between w-full">
                 <ul className='flex flex-col lg:flex-row'>{this.leftMenu()}</ul>
                 <ul className='flex flex-col lg:flex-row'>{this.rightMenu()}</ul>
             </div>
