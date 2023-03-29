544c1de21f3eef8516b47e43777ea7300ccd7676
steven <steven@StefanoPugg>
2023-03-29 10:00:24 +0200

    Added form to ContactsComponent.tsx

--- src/components/pages/contacts/ContactsComponent.tsx
+++ src/components/pages/contacts/ContactsComponent.tsx
@@ -9,7 +9,26 @@ export default class ContactsComponent extends Component<any,any>{
 
     render(): React.ReactNode {
         return (
-            <TitleComponent title="Pagina dei contatti" />
+            <>
+                <TitleComponent title="Pagina dei contatti" />
+                <div id="contacts-container">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <label htmlFor="">Nome</label>
+                        <input id="name" type="text" />
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <label htmlFor="">Email</label>
+                        <input id="email" type="email" />
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <label htmlFor="">Messaggio</label>
+                        <textarea id="message"></textarea>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                    </div>
+                </div>
+            </>
+            
         )
     }
 }
