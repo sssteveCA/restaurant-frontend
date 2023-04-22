a66c40463802457b6ab1bf59617ffb58a14c7b49
stepp <stepp@mail.com>
2023-04-22 19:37:51 +0200

    Updated TwoButtonsComponent.tsx style

--- src/components/partials/buttons/twobuttons/TwoButtonsComponent.tsx
+++ src/components/partials/buttons/twobuttons/TwoButtonsComponent.tsx
@@ -15,13 +15,13 @@ class TwoButtonsComponent extends Component<Types.TwoButtonsProps, any> {
 
     render(): React.ReactNode {
         return (
-    <div className="grid grid-cols-12 gap-4">
-        <div className='columns-12 md:columns-2 md:col-start-3'>
+    <div className="grid grid-cols-12 grid-rows-1 gap-4">
+        <div className='col-span-6 text-center'>
             <button className={this.props.buttons[0].classes} type="button" onClick={this.onPrimaryButtonClick}>
                 {this.props.buttons[0].text}
             </button>
         </div>
-        <div className='columns-12 md:columns-2 md:col-start-7'>
+        <div className='col-span-6 text-center'>
             <button className={this.props.buttons[1].classes} type="button" >
                 {this.props.buttons[1].text}
             </button>
