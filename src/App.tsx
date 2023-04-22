f552bfa7dd76d07e9e88754ce0a37727ac171416
stepp <stepp@mail.com>
2023-04-22 18:31:49 +0200

    Added tw_input.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './tw_output.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import ContainerComponent from './components/pages/ContainerComponent';
