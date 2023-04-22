83f9726befd1e09545ef504faccce8310e56f5e7
stepp <stepp@mail.com>
2023-04-22 17:34:48 +0200

    Added src/constants/paths.tsx

---
+++ src/constants/paths.tsx
@@ -0,0 +1,6 @@
+
+export namespace Paths{
+    export const URL_BASE: string = "http://localhost:8080"
+    export const URL_LOGIN: string = "/login";
+    export const URL_SUBSCRIBE: string = "/register";
+}
