725276ece2479ae2ad402d04589f24d04e9e43a2
stepp <stepp@mail.com>
2023-04-27 11:51:33 +0200

    Updated our_team array to AboutComponent.tsx

--- src/components/pages/about/AboutComponent.tsx
+++ src/components/pages/about/AboutComponent.tsx
@@ -3,44 +3,46 @@ import TitleComponent from '../../partials/text/title/TitleComponent'
 import './AboutComponent.sass'
 import Types from '../../../constants/types'
 import TeamItemComponent from './teamitem/TeamItemComponent'
-import tizio from '../../../assets/images/team/tizio.jpg'
-import caio from '../../../assets/images/team/caio.jpg'
-import sempronio from '../../../assets/images/team/sempronio.jpg'
-import mevio from '../../../assets/images/team/mevio.jpg'
+import alessandro_borghese from '../../../assets/images/team/alessandro_borghese.jpg'
+import carlo_cracco from '../../../assets/images/team/carlo_cracco.jpg'
+import giorgio_locatelli from '../../../assets/images/team/giorgio_locatelli.jpg'
+import antonio_cannavaciuolo from '../../../assets/images/team/antonio_cannavaciuolo.jpg'
 
 const AboutComponent = () => {
 
   let our_team: Types.TeamItemProps[] = [
     {
       image: {
-        classes: 'order-1', name: 'Tizio', url: tizio
+        classes: 'order-1', name: 'Alessandro Borghese', url: alessandro_borghese
       },
       text: {
-        classes: 'order-2', description: 'Titolare', title: 'Tizio'
+        classes: 'order-2', description: `È uno dei cuochi più famosi d’Italia e protagonista indiscusso del successo della cucina in televisione, è riuscito a far avvicinare e appassionare un pubblico sempre più variegato all’arte della cucina italiana.
+        Esempio di un nuovo approccio alla materia prima, ha rinnovato il concetto di cultura e professionalità nel settore della ristorazione.
+        Definito lo chef «Rock&Social», è stato premiato più volte per la sua cucina, legata alla qualità, dove tradizione e innovazione diventano metodo, stile, arte e gusto.`, title: 'Alessandro Borghese'
       }
     },
     {
       image: {
-        classes: 'order-2', name: 'Caio', url: caio
+        classes: 'order-2', name: 'Carlo Cracco', url: carlo_cracco
       },
       text: {
-        classes: 'order-1', description: 'Chef', title: 'Caio'
+        classes: 'order-1', description: `È il più famoso tra i grandi chef usciti dalle feconda covata di Gualtiero Marchesi. Carlo Cracco, vicentino classe 1965, deve la propria notorietà presso il vasto pubblico al fatto di essere diventato anche un personaggio tv, di quelli ambiti da cacciatori (e soprattutto cacciatrici, visto il crescente fascino acquistato con l’età) di selfie. Ma i buongustai non sono attirati da Masterchef, quanto dalla sua cucina, forse la più creativa tra quelle celebrate a Milano.`, title: 'Carlo Cracco'
       }
     },
     {
       image: {
-        classes: 'order-1', name: 'Sempronio', url: sempronio
+        classes: 'order-1', name: 'Giorgio Locatelli', url: giorgio_locatelli
       },
       text: {
-        classes: 'order-2', description: 'Cameriere', title: 'Sempronio'
+        classes: 'order-2', description: `È uno chef italiano di fama internazionale, noto per la sua cucina raffinata, basata sulla tradizione culinaria italiana. Nato a Corgeno, in Lombardia, nel 1963, Locatelli ha iniziato la sua carriera in cucina da adolescente, lavorando poi in alcuni dei ristoranti più prestigiosi d'Italia.`, title: 'Giorgio Locatelli'
       }
     },
     {
       image: {
-        classes: 'order-2', name: 'Mevio', url: mevio
+        classes: 'order-2', name: 'Antonio Cannavacciuolo', url: antonio_cannavaciuolo
       },
       text: {
-        classes: 'order-1', description: 'Sommelier', title: 'Mevio'
+        classes: 'order-1', description: `Nato il 16 aprile 1975 a Vico Equense (dove nasce anche Gino D’Acampo), in provincia di Napoli. Studia all’Istituto Alberghiero “F.De Gennaro” del suo paese natale, dove ha insegnato anche il padre. Dopo il diploma, nel 1994, inizia a lavorare come cuoco in Penisola Sorrentina. Successivamente iniziano le esperienze all’estero: tra cui quella in Alsazia, a Strasburgo. Ritorna in Italia e lavora all’Hotel Quisisana a Capri nel tempo in cui Gualtiero Marchesi ne era il consulente. Villa Crespi arriva solo nel 1999, dimora storica che prende con sua moglie e grazie al quale riceve ben due stelle Michelin (nel 2003 e nel 2006).`, title: 'Antonio Cannavaciuolo'
       }
     }
   ];
--- src/components/pages/about/teamitem/TeamItemComponent.tsx
+++ src/components/pages/about/teamitem/TeamItemComponent.tsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import Types from '../../../../constants/types'
+import './TeamItemComponent.sass'
 
 export default class TeamItemComponent extends Component<Types.TeamItemProps, any> {
 
   render() {
     return (
-      <div className='flex flex-col md:flex-row my-5 justify-center md:justify-between'>
-        <div className={`team-item-image pt-5 md:pt-0 w-full md:w-1/2 ${this.props.image.classes}`}>
+      <div className='team-item-div flex flex-col md:flex-row my-5 justify-center md:justify-between bg-white/[.7]'>
+        <div className={`team-item-image flex-auto ${this.props.image.classes}`}>
             <img src={this.props.image.url} alt={this.props.image.name} title={this.props.image.name} className='m-auto' />
         </div>
-        <div className={`team-item-text flex flex-col w-full md:w-1/2 ${this.props.text.classes}`}>
+        <div className={`team-item-text flex flex-col flex-auto p-5 ${this.props.text.classes}`}>
             <div className='title text-center text-3xl font-bold'>{this.props.text.title}</div>
             <div className='description'>{this.props.text.description}</div>
         </div>
