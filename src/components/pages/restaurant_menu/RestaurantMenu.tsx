89ac204c6425b90eeb6dcdcb13a9cdafd92fda39
stepp <stepp@mail.com>
2023-04-27 14:54:50 +0200

    Added menu request to RestaurantMenu.tsx componentDidMount method

--- src/components/pages/restaurant_menu/RestaurantMenu.tsx
+++ src/components/pages/restaurant_menu/RestaurantMenu.tsx
@@ -1,16 +1,44 @@
 import React, { Component } from "react";
 import TitleComponent from "../../partials/text/title/TitleComponent";
+import { MenuRequest, MenuRequestType } from "../../../requests/menu_request";
+import Types from "../../../constants/types";
+import SpinnerComponent from "../../partials/spinner/SpinnerComponent";
+import { Keys } from "../../../constants/keys";
+import MessageComponent from "../../partials/text/message/MessageComponent";
 
-export default class RestaurantMenu extends Component<any,any>{
+export default class RestaurantMenu extends Component<any,Types.RestaurantMenuState>{
     
     constructor(props: any){
         super(props)
+        this.state = {
+            menu: [], message: '', spinner_show: false
+        }
+    }
+
+    componentDidMount(): void {
+        let mr_data: MenuRequestType = {
+            url: ''
+        }
+        let mr: MenuRequest = new MenuRequest(mr_data)
+        this.setState({spinner_show: true})
+        mr.getMenu().then(obj => {
+            this.setState({spinner_show: false})
+            if(obj[Keys.KEY_DONE]){
+                this.setState({menu: obj['menu']})
+            }
+            else this.setState({message: obj[Keys.KEY_MESSAGE]})
+        })
     }
 
     render(): React.ReactNode {
         return (
             <>
                 <TitleComponent title="Pagina menu ristorante" />
+                <SpinnerComponent visible={this.state.spinner_show} />
+                <MessageComponent message={this.state.message} />
+                {
+                    this.state.menu.map((menu_single,i) => <div key={i} />)
+                }
             </>
         )
     }
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -4,5 +4,6 @@ export namespace Paths{
     export const URL_DISHES_ALL: string = "/dishes/all"
     export const URL_CONTACTS: string = "/contacts/support"
     export const URL_LOGIN: string = "/login";
+    export const URL_MENU: string = "/menu";
     export const URL_REGISTER: string = "/register";
 }
