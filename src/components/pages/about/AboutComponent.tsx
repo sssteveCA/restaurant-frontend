b398a3eb7e3a41e340a758cb934de2a304147f33
stepp <stepp@mail.com>
2023-04-27 11:18:15 +0200

    Added image to src/assets/images/team folder

--- src/components/pages/about/AboutComponent.tsx
+++ src/components/pages/about/AboutComponent.tsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import TitleComponent from '../../partials/text/title/TitleComponent'
 import './AboutComponent.sass'
+import Types from '../../../constants/types'
+import TeamItemComponent from './teamitem/TeamItemComponent'
+import tizio from '../../../assets/images/team/tizio.jpg'
+import caio from '../../../assets/images/team/caio.jpg'
+import sempronio from '../../../assets/images/team/sempronio.jpg'
+import mevio from '../../../assets/images/team/mevio.jpg'
 
 const AboutComponent = () => {
+
+  let our_team: Types.TeamItemProps[] = [
+    {
+      image: {
+        classes: 'order-1', name: 'Tizio', url: tizio
+      },
+      text: {
+        classes: 'order-2', description: 'Titolare', title: 'Tizio'
+      }
+    },
+    {
+      image: {
+        classes: 'order-2', name: 'Caio', url: caio
+      },
+      text: {
+        classes: 'order-1', description: 'Chef', title: 'Caio'
+      }
+    },
+    {
+      image: {
+        classes: 'order-1', name: 'Sempronio', url: sempronio
+      },
+      text: {
+        classes: 'order-2', description: 'Cameriere', title: 'Sempronio'
+      }
+    },
+    {
+      image: {
+        classes: 'order-2', name: 'Mevio', url: mevio
+      },
+      text: {
+        classes: 'order-1', description: 'Sommelier', title: 'Mevio'
+      }
+    }
+  ];
+
   return (
     <>
       <TitleComponent title="Pagina chi siamo" />
       <div className='mt-10 flex flex-col'>
-
+        {
+          our_team.map((item,i) => ( <TeamItemComponent {...item} />))
+        }
       </div>
     </>
     
--- src/components/pages/about/teamitem/TeamItemComponent.tsx
+++ src/components/pages/about/teamitem/TeamItemComponent.tsx
@@ -6,7 +6,7 @@ export default class TeamItemComponent extends Component<Types.TeamItemProps, an
   render() {
     return (
       <div className='flex flex-col md:flex-row my-5'>
-        <div className={`team-item-image ${this.props.image.classes}`}>
+        <div className={`team-item-image pt-5 md:pt-0 ${this.props.image.classes}`}>
             <img src={this.props.image.url} alt={this.props.image.classes} title={this.props.image.classes} />
         </div>
         <div className={`team-item-text flex flex-col ${this.props.text.classes}`}>
